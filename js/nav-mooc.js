3dfd3fe147dcf4cb2eba5703069a1e55d2647a88
ThomasViennet <viennet.t@gmail.com>
2023-03-25 20:28:51 +0100

    improve the style of navigation

--- js/nav-mooc.js
+++ js/nav-mooc.js
@@ -1,5 +1,6 @@
 function openNav() {
     document.getElementById("navMooc").style.width = "100%";
+    // document.getElementById("navMooc a").style.color = "#0a2463";
 }
 
 function closeNav() {
--- views/nav-mooc.php
+++ views/nav-mooc.php
@@ -6,11 +6,8 @@
         <a href="https://referencime.fr/formation-seo-gratuite/quest-ce-que-le-seo/">
             <h2 class="
             <?php
-            if (in_array("1", $lessons_id, true)) {
+            if (in_array("1", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
             ?>">Découvrez l'univers du SEO</h2>
         </a>
 
@@ -19,55 +16,40 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("2", $lessons_id, true)) {
+                if (in_array("2", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Le concept E-E-A-T de Google</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("3", $lessons_id, true)) {
+                if (in_array("3", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Le SEO white et black hat</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("4", $lessons_id, true)) {
+                if (in_array("4", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Le SEA</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("5", $lessons_id, true)) {
+                if (in_array("5", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Le SXO</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("6", $lessons_id, true)) {
+                if (in_array("6", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Le SMO</li>
             </a>
 
@@ -76,11 +58,8 @@
         <a href="#">
             <h2 class="
             <?php
-            if (in_array("7", $lessons_id, true)) {
+            if (in_array("7", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
             ?>">Découvrez le fonctionnennement d'un moteur de recherche</h2>
         </a>
 
@@ -90,33 +69,24 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("8", $lessons_id, true)) {
+                if (in_array("8", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Histoire de la recherche d’informations</li>
             </a>
             <ol>
                 <a href="#">
                     <li class="
                 <?php
-                if (in_array("9", $lessons_id, true)) {
+                if (in_array("9", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Historique des parts de marché des moteurs de recherche</li>
                 </a>
 
                 <a href="#">
                     <li class="
                 <?php
-                if (in_array("10", $lessons_id, true)) {
+                if (in_array("10", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Historique des algorithmes de Google</li>
                 </a>
             </ol>
@@ -124,22 +94,16 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("11", $lessons_id, true)) {
+                if (in_array("11", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement du PageRank</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("12", $lessons_id, true)) {
+                if (in_array("12", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement du Learning to rank</li>
             </a>
 
@@ -147,11 +111,8 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("13", $lessons_id, true)) {
+                if (in_array("13", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement du Transition rank</li>
             </a>
 
@@ -159,55 +120,40 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("14", $lessons_id, true)) {
+                if (in_array("14", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement de Google BERT</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("15", $lessons_id, true)) {
+                if (in_array("15", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement de Google Penguin</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("16", $lessons_id, true)) {
+                if (in_array("16", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement de Google Panda</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("17", $lessons_id, true)) {
+                if (in_array("17", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement du Duplicate content</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("18", $lessons_id, true)) {
+                if (in_array("18", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Fonctionnennement du Knowledge Graph de Google</li>
             </a>
 
@@ -216,11 +162,8 @@
         <a href="#">
             <h2 class="
             <?php
-            if (in_array("19", $lessons_id, true)) {
+            if (in_array("19", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
             ?>">Optimisations on-site</h2>
         </a>
 
@@ -229,110 +172,80 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("20", $lessons_id, true)) {
+                if (in_array("20", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser l’indexation des crawlers</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("21", $lessons_id, true)) {
+                if (in_array("21", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser les pages web</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("22", $lessons_id, true)) {
+                if (in_array("22", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser les images</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("23", $lessons_id, true)) {
+                if (in_array("23", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser les vidéos</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("24", $lessons_id, true)) {
+                if (in_array("24", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser le référencement local</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("25", $lessons_id, true)) {
+                if (in_array("25", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser les sites e-commerce</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("26", $lessons_id, true)) {
+                if (in_array("26", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser la version mobile d’un site web</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("27", $lessons_id, true)) {
+                if (in_array("27", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser les fichiers texte</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("28", $lessons_id, true)) {
+                if (in_array("28", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser les actualités</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("29", $lessons_id, true)) {
+                if (in_array("29", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Optimiser les podcasts</li>
             </a>
 
@@ -341,11 +254,8 @@
         <a href="#">
             <h2 class="
             <?php
-            if (in_array("30", $lessons_id, true)) {
+            if (in_array("30", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
             ?>">Créer une stratégie SEO pour Google</h2>
         </a>
 
@@ -354,11 +264,8 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("31", $lessons_id, true)) {
+                if (in_array("31", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Créer une liste de mots-clés</li>
             </a>
             <ol>
@@ -366,22 +273,16 @@
 
                     <li class="
                 <?php
-                if (in_array("32", $lessons_id, true)) {
+                if (in_array("32", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Le triangle d’or de Google</li>
                 </a>
 
                 <a href="#">
                     <li class="
                 <?php
-                if (in_array("33", $lessons_id, true)) {
+                if (in_array("33", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">La recherche universelle</li>
                 </a>
             </ol>
@@ -389,44 +290,32 @@
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("34", $lessons_id, true)) {
+                if (in_array("34", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Utiliser la sémantique en SEO</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("35", $lessons_id, true)) {
+                if (in_array("35", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Créer un cocon sémantique</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("36", $lessons_id, true)) {
+                if (in_array("36", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Créer un maillage interne</li>
             </a>
 
             <a href="#">
                 <li class="
                 <?php
-                if (in_array("37", $lessons_id, true)) {
+                if (in_array("37", $lessons_id, true))
                     echo 'lessonCompleted';
-                } else {
-                    echo 'lessonUncompleted';
-                }
                 ?>">Créer une campagne de netlinking</li>
             </a>
 
@@ -435,38 +324,27 @@
         <a href="#">
             <h2 class="
             <?php
-            if (in_array("38", $lessons_id, true)) {
+            if (in_array("38", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
             ?>">Ressources utiles</h2>
         </a>
 
         <ol>
             <li class="
             <?php
-            if (in_array("11", $lessons_id, true)) {
+            if (in_array("11", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
+
             ?>">Liste de formations et MOOC</li>
             <li class="
             <?php
-            if (in_array("11", $lessons_id, true)) {
+            if (in_array("11", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
             ?>">Tour d’horizon des outils du marché</li>
             <li class="
             <?php
-            if (in_array("11", $lessons_id, true)) {
+            if (in_array("11", $lessons_id, true))
                 echo 'lessonCompleted';
-            } else {
-                echo 'lessonUncompleted';
-            }
             ?>">Où travailler</li>
         </ol>
     </div>
