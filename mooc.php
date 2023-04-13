383867e89559df1c9afc705178024f55890be756
ThomasViennet <viennet.t@gmail.com>
2023-04-13 10:22:29 +0200

    clean code

--- mooc.php
+++ mooc.php
@@ -24,7 +24,7 @@ use Mooc\Controllers\Quiz\Controller_Quiz;
 use Mooc\Controllers\Lesson\Controller_Lesson;
 
 register_activation_hook(__FILE__, array(new Controller_Init(), 'createTables'));
-add_action('init', array(new Controller_Init(), 'init'));//Contains the filters and actions hooks
+add_action('init', array(new Controller_Init(), 'init')); //Contains the filters and actions hooks
 
 
 //Shortcodes
@@ -33,7 +33,9 @@ function registration()
 {
     if (!is_admin()) {
         if (!is_user_logged_in()) {
+            ob_start();
             Controller_User::displayRegistrationForm();
+            return ob_get_clean();
         }
     }
 }
@@ -41,9 +43,13 @@ function registration()
 add_shortcode('nav_mooc', 'navMooc');
 function navMooc()
 {
+    if (!is_customize_preview()) {
         if (is_user_logged_in()) {
+            ob_start();
             Controller_NavMooc::display();
+            return ob_get_clean();
         }
+    }
 }
 
 add_shortcode('lesson_button', 'lessonButton');
@@ -64,7 +70,9 @@ function lessonButton()
                     }
                 }
             }
+            ob_start();
             Controller_Lesson::displayButton($user->ID, basename(get_permalink()));
+            return ob_get_clean();
         }
     }
 }
@@ -79,9 +87,9 @@ function quiz()
         $quiz_id = 0; //Will be usefull when there will are a CRUD for quizzes
 
         if (isset($_GET['action']) && $_GET['action'] == 'submit') {
-            
+
             //Need to check if the user has answered all the questions
-            
+
             $answers = [
                 $_POST['question1'],
                 $_POST['question2'],
@@ -95,10 +103,12 @@ function quiz()
                 $_POST['question10']
             ];
 
-            (new Controller_Quiz())->saveAnswers($user->ID, $quiz_id, $_GET['quiz_name'], $answers);
+            Controller_Quiz::saveAnswers($user->ID, $quiz_id, $_GET['quiz_name'], $answers);
         }
-        (new Controller_Quiz())->viewQuiz($user->ID, $_GET['quiz_name']);
+        ob_start();
+        Controller_Quiz::viewQuiz($user->ID, $_GET['quiz_name']);
+        return ob_get_clean();
     } else {
-        (new Controller_User)->displayRegistrationForm();
+        Controller_User::displayRegistrationForm();
     }
 }
