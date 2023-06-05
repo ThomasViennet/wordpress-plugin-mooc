6c374dbddb21c0a91fc1a71af05f008e859b8bdc
ThomasViennet <viennet.t@gmail.com>
2023-06-05 19:37:02 +0200

    clean css form and button

--- views/registration.php
+++ views/registration.php
@@ -1,10 +1,8 @@
-<div class="padding border-radius box-shadow padding has-text-align-center">
+<form action="<?= site_url('wp-login.php?action=register', 'login_post') ?>" method="post" class="has-text-align-center">
     <h2>S'inscrire à la formation</h2>
-    <form action="<?= site_url('wp-login.php?action=register', 'login_post') ?>" method="post">
-        <input type="text" name="user_login" placeholder="Identifiant" id="user_login" style="margin:5px 0" />
-        <input type="email" name="user_email" placeholder="Email" id="user_email" style="margin:5px 0" />
-        <?php do_action('register_form'); ?>
-        <input type="submit" value="Valider l'inscription" id="register" style="margin:5px 0" />
-        <p><a href="<?= site_url('wp-login.php') ?>">J'ai déjà un compte</a></p>
-    </form>
-</div>
+    <input type="text" name="user_login" placeholder="Identifiant" id="user_login" style="margin:5px 0" />
+    <input type="email" name="user_email" placeholder="Email" id="user_email" style="margin:5px 0" />
+    <?php do_action('register_form'); ?>
+    <input type="submit" value="Valider l'inscription" id="register" style="margin:5px 0" />
+    <p><a href="<?= site_url('wp-login.php') ?>">J'ai déjà un compte</a></p>
+</form>
