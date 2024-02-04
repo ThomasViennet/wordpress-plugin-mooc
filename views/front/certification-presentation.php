b4329ddd8a94b38a573d6040077e2b9d412f92df
ThomasViennet <viennet.t@gmail.com>
2024-02-04 15:53:13 +1100

    presentation certification

--- views/front/certification-presentation.php
+++ views/front/certification-presentation.php
@@ -1,88 +1,40 @@
 <p>Cette certification SEO gratuite a pour objectif de vous aider à légitimer votre niveau de connaissance en référencement naturel.</p>
 
-
-
 <h2 class="wp-block-heading">Prérequis pour obtenir la certification</h2>
 
-
-
-<p>Vous pouvez passer la certification quand vous le souhaitez et gratuitement et si vous avez suivi la formation SEO gratuite, vous devriez être en mesure de répondre à toutes les questions.</p>
-
-
+<p>
+    Vous pouvez passer la certification gratuitement quand vous le souhaitez.<br>
+    Si vous avez suivi la <a href="https://referencime.fr/formation-seo-gratuite/">formation SEO en ligne gratuite</a>, vous devriez être en mesure de répondre à toutes les questions.
+</p>
 
 <h2 class="wp-block-heading">Connaissance évaluées</h2>
 
-
-
-<ol>
-    <li><strong>SEO (Search Engine Optimization)</strong> : Comprendre les principes de base du référencement naturel.</li>
-
-
-
-    <li><strong>SEA (Search Engine Advertising)</strong> : Connaissances fondamentales en publicité sur les moteurs de recherche.</li>
-
-
-
-    <li><strong>SMO (Social Media Optimization)</strong> : Stratégies d’optimisation pour les réseaux sociaux.</li>
-
-
-
-    <li><strong>SXO (Search Experience Optimization)</strong> : Optimisation de l’expérience utilisateur dans le contexte des recherches en ligne.</li>
-
-
-
-    <li><strong>Fonctionnement des Moteurs de Recherche</strong> : Comprendre comment les moteurs de recherche indexent et classent les sites.</li>
-
-
-
-    <li><strong>SEO On-Site</strong> : Techniques d’optimisation sur le site lui-même.</li>
-
-
-
-    <li><strong>Conception d’une Stratégie SEO</strong> : Planification et mise en œuvre d’une stratégie SEO efficace.</li>
-</ol>
-
-
+<ul>
+    <li><strong>SEO (Search Engine Optimization)</strong> : Vous connaissez les principes du référencement naturel.</li>
+    <li><strong>SEA (Search Engine Advertising)</strong> : Vous savez vous servir du SEA pour le SEO.</li>
+    <li><strong>SMO (Social Media Optimization)</strong> : Vous savez vous servir des réseaux sociaux pour le SEO.</li>
+    <li><strong>SXO (Search Experience Optimization)</strong> : Vous savez améliorer l’expérience utilisateur pour le SEO.</li>
+    <li><strong>Fonctionnement des Moteurs de Recherche</strong> : Vous savez comment les moteurs de recherche fonctionnent.</li>
+    <li><strong>SEO On-Site</strong> : Vous connaissez les techniques d’optimisation on-site.</li>
+    <li><strong>Conception d’une Stratégie SEO</strong> : Vous savez créer et mettre en oeuvre une stratégie SEO efficace.</li>
+</ul>
 
 <h2 class="wp-block-heading">Comment obtenir la certification ?</h2>
 
-
-
 <ol>
-    <li>Créer un compte gratuitement</li>
-
-
-
+    <li><a href="#inscription_formation">Créer un compte gratuitement</a></li>
     <li>Répondre aux 40 questions à choix multiples</li>
-
-
-
     <li>Obtenir un score de 80% de réussite</li>
 </ol>
 
-
-
 <h3 class="wp-block-heading">Système de notation</h3>
-
-
 
 <ul>
     <li>1 point par bonne réponse</li>
-
-
-
     <li>-1 point par mauvaise réponse</li>
-
-
-
     <li>Toutes les réponses peuvent être bonnes</li>
-
-
-
     <li>Toutes les réponses peuvent être mauvaises</li>
 </ul>
-
-
 
 <blockquote class="wp-block-quote">
     <p>Exemple de calcul du taux de réussite : S’il y a 50 bonnes réponses parmi les 40 questions et que vous choisissez 40 bonnes réponses et 10 mauvaises réponses, vous obtenez un score de : 40 points – 10 points = 30 points, soit 60% de taux de réussite.</p>
@@ -92,73 +44,99 @@
 
 <h3 class="wp-block-heading">Validation de la certification</h3>
 
+<p>Une fois la certification réussie, une URL unique sera générée sur notre site, attestant de votre réussite. Ce lien peut être utilisé comme preuve de vos connaissances en SEO, que ce soit pour des opportunités d’emploi, des promotions ou simplement pour enrichir votre profil professionnel.</p>
 
-
-<p>Une fois la certification réussie, un URL unique sera généré sur notre site, attestant de votre réussite. Ce lien peut être utilisé comme preuve de votre compétence en SEO, que ce soit pour des opportunités d’emploi, des promotions, ou simplement pour enrichir votre profil professionnel.</p>
-
-
-
-<h2 class="wp-block-heading">FAQ </h2>
-
-
-
-Question améliorée : "Quelles sont les options si je ne réussis pas l'examen ?"
-
-Réponse améliorée : "En cas d'échec à l'examen, vous avez la possibilité de retenter le questionnaire une fois toutes les 24 heures."
-
-Question améliorée : "Quelles sont les meilleures méthodes pour préparer l'examen ?"
-
-Réponse améliorée : "Je recommande vivement de parcourir attentivement les différentes leçons proposées dans notre formation SEO gratuite pour une préparation optimale."
-
-Question améliorée : "Quelle est la durée de validité de la certification obtenue ?"
-
-Réponse améliorée : "Votre certification restera valide indéfiniment. Cependant, il est important de noter que le domaine du SEO est en constante évolution. Ainsi, les connaissances acquises peuvent devenir obsolètes après quelques années."
-
-Question améliorée : "Comment puis-je obtenir de l'aide si j'ai des questions supplémentaires ?"
-
-Réponse améliorée : "Pour toute question complémentaire, n'hésitez pas à me contacter par courriel à l'adresse suivante : contact@referencime.fr."
-
+<h2 class="wp-block-heading">FAQ</h2>
+<div class="schema-faq wp-block-yoast-faq-block fcp-faq">
+    <div class="schema-faq-section" id="faq-question-1">
+        <strong class="schema-faq-question">Que faire si je ne réussis pas l'examen ?</strong>
+        <p class="schema-faq-answer">En cas d'échec à l'examen, vous avez la possibilité de retenter le questionnaire une fois toutes les 24 heures.</p>
+    </div>
+    <div class="schema-faq-section" id="faq-question-2">
+        <strong class="schema-faq-question">Comment se préparer l'examen ?</strong>
+        <p class="schema-faq-answer">Je recommande vivement de parcourir attentivement les différentes leçons proposées dans notre formation SEO gratuite pour une préparation optimale.</p>
+    </div>
+    <div class="schema-faq-section" id="faq-question-3">
+        <strong class="schema-faq-question">Quelle est la durée de validité de la certification ?</strong>
+        <p class="schema-faq-answer">Votre certification restera valide indéfiniment. Cependant, il est important de noter que le domaine du SEO est en constante évolution. Ainsi, les connaissances acquises peuvent devenir obsolètes après quelques années.</p>
+    </div>
+    <div class="schema-faq-section" id="faq-question-4">
+        <strong class="schema-faq-question">Comment puis-je obtenir de l'aide si j'ai des questions supplémentaires ?</strong>
+        <p class="schema-faq-answer">Pour toute question complémentaire, n'hésitez pas à me contacter par email à l'adresse suivante : <a href="mailto:contact@referencime.fr">contact@referencime.fr</a>.</p>
+    </div>
+</div>
 
 
 <h2 class="wp-block-heading">Principales ressources complémentaires</h2>
 
+<!-- wp:media-text {"mediaId":6544,"mediaLink":"https://referencime.fr/consultant-seo-freelance/attachment/choisir-un-consultant-seo-freelance/","mediaType":"image","mediaWidth":30,"imageFill":true,"className":"box-shadow border-radius"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-image-fill box-shadow border-radius" style="grid-template-columns:30% auto">
+    <figure class="wp-block-media-text__media" style="background-image:url(https://referencime.fr/wp-content/uploads/2023/05/Choisir-un-consultant-SEO-freelance-scaled-e1685218969634-1024x833.jpeg);background-position:50% 50%"><img src="https://referencime.fr/wp-content/uploads/2023/05/Choisir-un-consultant-SEO-freelance-scaled-e1685218969634-1024x833.jpeg" alt="Choisir un consultant SEO freelance" class="wp-image-6544 size-full" /></figure>
+    <div class="wp-block-media-text__content"><!-- wp:heading {"level":3} -->
+        <h3 class="wp-block-heading" id="h-chapitre-1-qu-est-ce-que-le-seo">Chapitre 1 : <a href="https://referencime.fr/formation-seo-gratuite/quest-ce-que-le-seo/">Qu'est-ce que le SEO ?</a></h3>
+        <!-- /wp:heading -->
 
+        <!-- wp:paragraph -->
+        <p><em>Le SEO est l'un des principaux leviers du marketing digital. Découvrez ce levier et les sujets qui gravitent autour.</em></p>
+        <!-- /wp:paragraph -->
+    </div>
+</div>
+<!-- /wp:media-text -->
 
-<ul>
-    <li>Qu’est-ce que le SEO</li>
+<!-- wp:media-text {"mediaId":7731,"mediaLink":"https://referencime.fr/comment-fonctionne-un-moteur-de-recherche-4/","mediaType":"image","mediaWidth":30,"imageFill":true,"className":"box-shadow border-radius"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-image-fill box-shadow border-radius" style="grid-template-columns:30% auto">
+    <figure class="wp-block-media-text__media" style="background-image:url(https://referencime.fr/wp-content/uploads/2023/07/comment-fonctionne-un-moteur-de-recherche.jpg);background-position:50% 50%"><img src="https://referencime.fr/wp-content/uploads/2023/07/comment-fonctionne-un-moteur-de-recherche.jpg" alt="Comment fonctionne un moteur de recherche" class="wp-image-7731 size-full" /></figure>
+    <div class="wp-block-media-text__content"><!-- wp:heading {"level":3} -->
+        <h3 class="wp-block-heading" id="chapitre-2-comment-fonctionne-un-moteur-de-recherche-tel-que-google">Chapitre 2 : <a href="https://referencime.fr/formation-seo-gratuite/comment-fonctionne-un-moteur-de-recherche/">Comment fonctionne un moteur de recherche tel que Google ?</a></h3>
+        <!-- /wp:heading -->
 
+        <!-- wp:paragraph -->
+        <p><em>Pour <strong>maîtriser l'environnement dans lequel vous évoluez</strong>, vous devez savoir comment fonctionne un moteur de recherche.</em></p>
+        <!-- /wp:paragraph -->
+    </div>
+</div>
+<!-- /wp:media-text -->
 
+<!-- wp:media-text {"mediaId":7733,"mediaLink":"https://referencime.fr/formation-seo-gratuite/attachment/optimisation-seo-tehnique-2/","mediaType":"image","mediaWidth":30,"imageFill":true,"className":"box-shadow border-radius"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-image-fill box-shadow border-radius" style="grid-template-columns:30% auto">
+    <figure class="wp-block-media-text__media" style="background-image:url(https://referencime.fr/wp-content/uploads/2023/07/Optimisation-SEO-tehnique.jpg);background-position:50% 50%"><img src="https://referencime.fr/wp-content/uploads/2023/07/Optimisation-SEO-tehnique.jpg" alt="Optimisations SEO technique" class="wp-image-7733 size-full" /></figure>
+    <div class="wp-block-media-text__content"><!-- wp:heading {"level":3} -->
+        <h3 class="wp-block-heading" id="chapitre-2-comment-fonctionne-un-moteur-de-recherche-tel-que-google">Chapitre 3 : <a href="https://referencime.fr/formation-seo-gratuite/toutes-les-optimisations-pour-les-moteurs-de-recherche-seo/">Optimiser le référencement on-site pour Google</a></h3>
+        <!-- /wp:heading -->
 
-    <li>Comment fonctionne un moteur de recherche </li>
+        <!-- wp:paragraph -->
+        <p><em>Découvrez tous les points clés pour optimiser le référencement on-site.</em></p>
+        <!-- /wp:paragraph -->
+    </div>
+</div>
+<!-- /wp:media-text -->
 
+<!-- wp:media-text {"mediaId":7739,"mediaLink":"https://referencime.fr/strategie-seo-3/","mediaType":"image","mediaWidth":30,"imageFill":true,"className":"box-shadow border-radius"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-image-fill box-shadow border-radius" style="grid-template-columns:30% auto">
+    <figure class="wp-block-media-text__media" style="background-image:url(https://referencime.fr/wp-content/uploads/2023/07/Strategie-SEO-1024x512.jpg);background-position:50% 50%"><img src="https://referencime.fr/wp-content/uploads/2023/07/Strategie-SEO-1024x512.jpg" alt="" class="wp-image-7739 size-full" /></figure>
+    <div class="wp-block-media-text__content"><!-- wp:heading {"level":3} -->
+        <h3 class="wp-block-heading" id="chapitre-2-comment-fonctionne-un-moteur-de-recherche-tel-que-google">Chapitre 4 : <a href="https://referencime.fr/formation-seo-gratuite/creer-une-strategie-seo-perenne-et-pertinente/"></a><a href="https://referencime.fr/formation-seo-gratuite/creer-une-strategie-seo-perenne-et-pertinente/">Créer une stratégie SEO pour Google</a></h3>
+        <!-- /wp:heading -->
 
-
-    <li>Optimisaiton SEO</li>
-
-
-
-    <li>Stratégie SEO</li>
-</ul>
+        <!-- wp:paragraph -->
+        <p><em>Commencez par vous initier à la création d'un audit d'un site web. Puis apprenez comment activer les leviers on-page et off-page.</em></p>
+        <!-- /wp:paragraph -->
+    </div>
+</div>
+<!-- /wp:media-text -->
 
 
 
 <div class="wp-block-group alignwide is-layout-constrained">
     <div class="wp-block-group__inner-container">
+
         <h2 class="wp-block-heading has-primary-color has-text-color" id="presentation-du-formateur">Présentation du formateur</h2>
-
-
 
         <div class="wp-block-media-text alignwide is-stacked-on-mobile no-margin-top no-margin-bottom img-width-50-align-center" style="grid-template-columns:15% auto">
             <figure class="wp-block-media-text__media"><img decoding="async" src="https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg" alt="Thomas Viennet" class="wp-image-7836 size-full"></figure>
             <div class="wp-block-media-text__content">
                 <p>Je m’appelle Thomas Viennet et après avoir travaillé plusieurs années dans le domaine du marketing digital, je me suis spécialisé pour devenir <a href="https://referencime.fr/consultant-seo-freelance/">freelance SEO</a>.</p>
-
-
-
                 <p>J’ai créé cette certification SEO gratuite pour aider les personnes qui souhaitent débuter dans le SEO et légitimer les connaissances théoriques qu’elles ont acquise en référencement naturel.</p>
-
-
-
                 <p>J’espère que cette certification pourra aider ceux qui souhaitent commencer une carrière dans le SEO ou simplement développer l’éventail de leurs compétences.</p>
             </div>
         </div>
