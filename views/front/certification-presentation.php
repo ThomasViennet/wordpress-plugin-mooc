59e0ff541c361807000aa0b3f0795d339e0d85b0
ThomasViennet <viennet.t@gmail.com>
2024-02-04 16:13:46 +1100

    amélioration présentation certification

--- views/front/certification-presentation.php
+++ views/front/certification-presentation.php
@@ -145,9 +145,17 @@
     </div>
 </div> -->
 
+<form action="https://referencime.fr/wp-login.php?action=register" method="post" class="has-text-align-center background-animation">
+    <h2 id="inscription-formation">S'inscrire à la formation</h2>
+    <input type="text" name="user_login" placeholder="Identifiant" id="user_login" style="margin:5px 0">
+    <input type="email" name="user_email" placeholder="Email" id="user_email" style="margin:5px 0">
+    <input type="submit" value="Valider l'inscription" id="register" style="margin:5px 0">
+    <p><a href="https://referencime.fr/wp-login.php" data-wpel-link="internal">J'ai déjà un compte</a></p>
+</form>
+
 <div class="wp-block-media-text alignwide is-stacked-on-mobile background-animation border-radius img-width-50-align-center" style="grid-template-columns:15% auto">
     <figure class="wp-block-media-text__media">
-    <img decoding="async" width="473" height="473" src="../wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg" alt="Thomas Viennet" class="wp-image-7836 size-medium entered lazyloaded" data-lazy-srcset="https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg 473w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-300x300.jpg 300w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-150x150.jpg 150w" data-lazy-sizes="(max-width: 473px) 100vw, 473px" data-lazy-src="../wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg" data-ll-status="loaded" sizes="(max-width: 473px) 100vw, 473px" srcset="https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg 473w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-300x300.jpg 300w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-150x150.jpg 150w">
+        <img decoding="async" width="473" height="473" src="../wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg" alt="Thomas Viennet" class="wp-image-7836 size-medium entered lazyloaded" data-lazy-srcset="https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg 473w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-300x300.jpg 300w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-150x150.jpg 150w" data-lazy-sizes="(max-width: 473px) 100vw, 473px" data-lazy-src="../wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg" data-ll-status="loaded" sizes="(max-width: 473px) 100vw, 473px" srcset="https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi.jpg 473w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-300x300.jpg 300w, https://referencime.fr/wp-content/uploads/2023/07/Thomas-Viennet-arrondi-150x150.jpg 150w">
     </figure>
     <div class="wp-block-media-text__content">
         <p>Auteur : Thomas Viennet - <a href="https://referencime.fr/consultant-seo-freelance/" rel="author" data-wpel-link="internal">Freelance SEO</a><br>
