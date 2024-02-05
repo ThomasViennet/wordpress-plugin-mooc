1747c1eed3401944249e4fc95d072b53765fa309
ThomasViennet <viennet.t@gmail.com>
2024-02-05 21:56:53 +1100

    fireworks

--- views/front/certificate-congratulations.php
+++ views/front/certificate-congratulations.php
@@ -261,6 +261,8 @@
 <div class="firework"></div>
 <div class="firework"></div>
 <div class="firework"></div>
+
+
 <div id="congratulations">
     <p>Bravo le certificat a été ajouté à votre profil public !</p>
     <p><a href="profil?user_id=<?= $user_id ?>">Voir mon profil</a></p>
